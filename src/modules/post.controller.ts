e81bb77f199bfd675cbdeacb4d084a69402a3e19
anamolhasan <mdanamolhasan1998@gmail.com>
2026-01-14 20:15:45 +0600

    authorId daynamic setup

--- src/modules/post.controller.ts
+++ src/modules/post.controller.ts
@@ -2,11 +2,17 @@ import { Request, Response } from "express"
 import { postService } from "./post.service"
 
 
+
 const createPost = async (req:Request, res:Response) => {
     try {
-        
+        const user = req.user
+        if(!user){
+            return res.status(400).json({
+                error:'Unauthorized!'
+            })
+        }
 
-        const result = await postService.createPost(req.body)
+        const result = await postService.createPost(req.body, user.id as string)
         res.status(201).json(result)
     } catch (error) {
         res.status(400).json({
