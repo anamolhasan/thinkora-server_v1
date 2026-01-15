c52a4ee247eb822bd7ee19f206d539d7d73b83e0
anamolhasan <mdanamolhasan1998@gmail.com>
2026-01-15 13:44:46 +0600

    serch filter learing

--- src/modules/post.controller.ts
+++ src/modules/post.controller.ts
@@ -1,27 +1,57 @@
-import { Request, Response } from "express"
-import { postService } from "./post.service"
+import { Request, Response } from "express";
+import { postService } from "./post.service";
+import { PostStatus } from "../../generated/prisma/enums";
 
-
-
-const createPost = async (req:Request, res:Response) => {
-    try {
-        const user = req.user
-        if(!user){
-            return res.status(400).json({
-                error:'Unauthorized!'
-            })
-        }
-
-        const result = await postService.createPost(req.body, user.id as string)
-        res.status(201).json(result)
-    } catch (error) {
-        res.status(400).json({
-            error:'Post creation failed',
-            details:error
-        })
+const createPost = async (req: Request, res: Response) => {
+  try {
+    const user = req.user;
+    if (!user) {
+      return res.status(400).json({
+        error: "Unauthorized!",
+      });
     }
-}
+
+    const result = await postService.createPost(req.body, user.id as string);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(400).json({
+      error: "Post creation failed",
+      details: error,
+    });
+  }
+};
+
+const getAllPost = async (req: Request, res: Response) => {
+  try {
+    const { search } = req.query;
+    const searchString = typeof search === "string" ? search : undefined;
+
+    const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+
+    // true or false
+    const isFeatured = req.query.isFeatured
+      ? req.query.isFeatured === "true"
+        ? true
+        : req.query.isFeatured === "false"
+        ? false
+        : undefined
+      : undefined;
+
+    const status = req.query.status as PostStatus | undefined
+
+    const authorId = req.query.authorId as string | undefined
+
+    const result = await postService.getAllPost({search:searchString, tags, isFeatured, status, authorId});
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({
+      error: "Post not find",
+      details: error,
+    });
+  }
+};
 
 export const postController = {
-    createPost
-}
+  createPost,
+  getAllPost,
+};
